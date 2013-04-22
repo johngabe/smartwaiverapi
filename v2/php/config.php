11319008973aeb99b8ee5a0f8173ff2eeecb6ac1
Mark Silliman <mark@silltech.com>
2013-04-22 15:53:52 -0700

    v2

---
+++ v2/php/config.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* API Version: 2.0
+ * This is the configuration file
+ */
+
+define('API_KEY',''); //CHANGE THIS TO YOUR API KEY: https://www.smartwaiver.com/m/rest/
+
+define('API_VERSION','v2'); //which version of the api should we use? (don't change this)
